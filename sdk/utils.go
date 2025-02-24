bf3fad590bbc0b91b0bffa4e8b091e3df4fa6373
jeseeqing <jeseeqing@gmail.com>
2025-02-24 11:12:51 +0800

    add GetOwnerAndNameFromId func

--- sdk/utils.go
+++ sdk/utils.go
@@ -1,16 +1,28 @@
 package sdk
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/abmpio/irisx/controllerx"
 	"github.com/casdoor/casdoor-go-sdk/casdoorsdk"
 	"github.com/kataras/iris/v12"
 )
 
-// 构建 owner/name 格式的id
+// setup owner/name format string
 func SetupNormalizedId(owner, name string) string {
 	return fmt.Sprintf("%s/%s", owner, name)
+}
+
+// split two Field from string ,separator value is /
+func GetOwnerAndNameFromId(id string) (string, string, error) {
+	tokens := strings.Split(id, "/")
+	if len(tokens) != 2 {
+		return "", "", errors.New("GetOwnerAndNameFromId() error, wrong token count for ID: " + id)
+	}
+
+	return tokens[0], tokens[1], nil
 }
 
 func MustGetCurrentUserInfo(ctx iris.Context) (*casdoorsdk.Claims, error) {
